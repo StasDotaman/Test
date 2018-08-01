e2d37ee1be597664ed42bef9946d01c3efca0262
Alisa <a.demennikova@andersenlab.com>
2018-08-01 13:46:06 +0300

    + stack Over Flow test

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,17 +1,31 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestClass {
+    WebDriver driver;
 
-
-    @Test
-    public void pnTest(){
-        WebDriver driver ;
+    @BeforeClass
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver",
                 "src/main/resources/chromedriver.exe");
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void stackOverFlowTest() {
         driver.get("https://stackoverflow.com/questions/12957635/sql-query-to-insert-datetime-in-sql-server");
+    }
+
+    @Test
+    public void pnTest() {
+        driver.get("https://pn.com.ua/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 }
